573f5ead25fda8ec8f59304ab6cb205753bcd897
Berk <berkbeleli@gmail.com>
2022-07-11 22:32:52 +0300

    Created Themes class
    Added custom color sets to app and created Themes structure inside this structure created static variables

---
+++ Charger/Charger/Global Constants/Themes.swift
@@ -0,0 +1,24 @@
+//
+//  Themes.swift
+//  Charger
+//
+//  Created by Berk Beleli on 2022-07-11.
+//
+
+import UIKit
+
+struct Themes {
+  // With this global stitic variables we can user less line of codes
+  static var colorDark = UIColor(named: "Dark")!
+  static var colorDark_2 = UIColor(named: "Dark-2")!
+  static var colorCharcoal = UIColor(named: "Charcoal")!
+  static var colorGrayScale = UIColor(named: "GrayScale")!
+  static var colorSecurity = UIColor(named: "SecurityPink")!
+  static var colorSelectedGreen = UIColor(named: "SelectedGreen")!
+  static var colorSolidWhite = UIColor(named: "SolidWhite")!
+  
+  static var fontExtraBold = UIFont(name: "Proxima Nova Extrabold", size: 19)!
+  static var fontBold = UIFont(name: "Proxima Nova Bold", size: 19)!
+  static var fontRegular = UIFont(name: "ProximaNova-Regular", size: 14)!
+  static var fontThin = UIFont(name: "Proxima Nova Thin", size: 14)!
+}
