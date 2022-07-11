ae57c9109680fac7309004e0dcfa9d81e7fd7065
Berk <berkbeleli@gmail.com>
2022-07-12 00:46:37 +0300

    Created LoginVM and AppConstants
    using AppConstants we added a constant DeviceUDID

---
+++ Charger/Charger/Global Constants/AppConstants.swift
@@ -0,0 +1,12 @@
+//
+//  AppConstants.swift
+//  Charger
+//
+//  Created by Berk Beleli on 2022-07-12.
+//
+
+import Foundation
+
+struct AppConstants {
+  static let deviceUDID: String = "7867867"
+}
---
+++ Charger/Charger/Login/ViewModel/LoginViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewModel.swift
+//  Charger
+//
+//  Created by Berk Beleli on 2022-07-12.
+//
+
+import Foundation
+
+class LoginViewModel {
+  
+  
+  
+  func loginRequest(email: String?) {
+    
+  }
+  
+  
+  
+}
