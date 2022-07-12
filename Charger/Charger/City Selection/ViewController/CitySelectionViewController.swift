effaa95bab1b4733eeed05c2e6c2291bd31d685f
Berk <berkbeleli@gmail.com>
2022-07-13 00:11:49 +0300

    View Designed and added textfile extension
    With setIcon UITextfield extension we can set right ıcon for our custom search bar text Field

---
+++ Charger/Charger/City Selection/ViewController/CitySelectionViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CitySelectionViewController.swift
+//  Charger
+//
+//  Created by Berk Beleli on 2022-07-13.
+//
+
+import UIKit
+
+class CitySelectionViewController: UIViewController {
+
+  @IBOutlet private weak var searchCityTextField: UITextField!
+  @IBOutlet private weak var indicatorView: UIView!
+  @IBOutlet private weak var cityTableView: UITableView!
+  
+  override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+}
--- Charger/Charger/Extensions/UITextField+Extension.swift
+++ Charger/Charger/Extensions/UITextField+Extension.swift
@@ -18,4 +18,22 @@ extension UITextField {
     self.layer.addSublayer(border)
     self.layer.masksToBounds = true
   }
+  
+  /// This function changes the image of the left side of the given text field
+  ///
+  ///  ```
+  /// @IBOutlet weak var usernameTextField: UITextField!
+  /// usernameTextField.setIcon(UIImage(named: "user")!)
+  /// ```
+  func setIcon(_ image: UIImage) {
+    let iconView = UIImageView(frame: CGRect(x: 10, y: 5, width: 20, height: 20)) // creates an icon view for  the left side of the textfield
+    iconView.image = image
+    iconView.tintColor = .lightGray
+    
+    let iconContainerView: UIView = UIView(frame: CGRect(x: 20, y: 0, width: 30, height: 30)) // creates a container for created icon
+    iconContainerView.addSubview(iconView) // add created icon to our container
+    leftView = iconContainerView // sets the left side of the textfield our created container
+    leftViewMode = .always // sets the left side of  the textfield always visible
+  }  
+  
 }
