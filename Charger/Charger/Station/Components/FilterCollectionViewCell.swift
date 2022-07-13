a9595c8b0a13056d73ee6c778b61649bfed45719
Berk <berkbeleli@gmail.com>
2022-07-14 01:14:33 +0300

    Created custom collection View cell

---
+++ Charger/Charger/Station/Components/FilterCollectionViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  FilterCollectionViewCell.swift
+//  Charger
+//
+//  Created by Berk Beleli on 2022-07-14.
+//
+
+import UIKit
+
+class FilterCollectionViewCell: UICollectionViewCell {
+  var deleteFilter: ((String) -> ())?
+  @IBOutlet private weak var bgFilterView: UIView!
+  @IBOutlet private(set) weak var filterLabel: UILabel!
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    
+    setupUI()
+  }
+  /// setup the ui
+  func setupUI() {
+    bgFilterView.layer.cornerRadius = ObjectConstants.filterbuttonBorderRadius
+    bgFilterView.layer.borderWidth = 2
+    bgFilterView.backgroundColor = Themes.colorDark
+    bgFilterView.layer.borderColor = Themes.colorSelectedGreen.cgColor
+  }
+  
+  @IBAction func deleteFilterButton(_ sender: UIButton) {
+    deleteFilter?("DeletedFilter")
+  }
+  
+}
---
+++ Charger/Charger/Station/Helpers/StationCollectionViewHelper.swift
@@ -0,0 +1,8 @@
+//
+//  StationCollectionViewHelper.swift
+//  Charger
+//
+//  Created by Berk Beleli on 2022-07-14.
+//
+
+import Foundation
