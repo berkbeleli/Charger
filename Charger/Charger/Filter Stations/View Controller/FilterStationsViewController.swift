2c17cd11940997e46ab05cb2c6d852bc9bd7469f
Berk <berkbeleli@gmail.com>
2022-07-13 22:26:12 +0300

    View Design Completed

---
+++ Charger/Charger/Filter Stations/View Controller/FilterStationsViewController.swift
@@ -0,0 +1,65 @@
+//
+//  FilterStationsViewController.swift
+//  Charger
+//
+//  Created by Berk Beleli on 2022-07-13.
+//
+
+import UIKit
+
+class FilterStationsViewController: UIViewController {
+
+  @IBOutlet weak var statusbarBackgroundView: UIView!
+  @IBOutlet weak var deviceTypeHeaderLabel: UILabel!
+  @IBOutlet weak var acDeviceTypeButton: UIButton!
+  
+  @IBOutlet weak var dcDeviceTypeButton: UIButton!
+  @IBOutlet weak var socketTypeHeaderLabel: UILabel!
+  
+  @IBOutlet weak var type2Button: UIButton!
+  
+  @IBOutlet weak var cscButton: UIButton!
+  
+  @IBOutlet weak var chademoButton: UIButton!
+  
+  @IBOutlet weak var distanceHeaderLabel: UILabel!
+  
+  @IBOutlet weak var distanceSlider: UISlider!
+  
+  @IBOutlet weak var fistSliderLabel: UILabel!
+  @IBOutlet weak var secondSliderLabel: UILabel!
+  
+  @IBOutlet weak var thirdSliderLabel: UILabel!
+  
+  
+  @IBOutlet weak var fourthSliderLabel: UILabel!
+  
+  
+  @IBOutlet weak var fifthSliderLabel: UILabel!
+  
+  @IBOutlet weak var servicesHeaderLabel: UILabel!
+  @IBOutlet weak var carParkButton: UIButton!
+  
+  @IBOutlet weak var buffetButton: UIButton!
+  
+  @IBOutlet weak var wifiButton: UIButton!
+  
+  @IBOutlet weak var filterButton: UIButton!
+  override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
