2995e386f733cf9a32023a83d7d76f2e7e3387ce
Berk <berkbeleli@gmail.com>
2022-07-13 14:17:14 +0300

    View design added

---
+++ Charger/Charger/Station/View Controller/StationViewController.swift
@@ -0,0 +1,19 @@
+//
+//  StationViewController.swift
+//  Charger
+//
+//  Created by Berk Beleli on 2022-07-13.
+//
+
+import UIKit
+
+class StationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+
+}
