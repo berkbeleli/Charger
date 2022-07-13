e9599103164896ef7157401b685a3875de61394a
Berk <berkbeleli@gmail.com>
2022-07-13 14:36:05 +0300

    Added Connections and setup the UI Components

--- Charger/Charger/Station/View Controller/StationViewController.swift
+++ Charger/Charger/Station/View Controller/StationViewController.swift
@@ -8,13 +8,70 @@
 import UIKit
 
 class StationViewController: UIViewController {
-
+  @IBOutlet weak var statusbarBackgroundView: UIView!
+  @IBOutlet weak var searchStationTextField: UITextField!
+  
+  @IBOutlet weak var filterCollectionView: UICollectionViewCell!
+  
+  @IBOutlet weak var resultLabel: UILabel!
+  
+  @IBOutlet weak var stationsTableView: UITableView!
   var cityName: String?
     override func viewDidLoad() {
         super.viewDidLoad()
-
-      
+      setupUI()
+      setupCustomSearchTextField()
     }
+  
+  /// Setup UI Elements
+  func setupUI(){
+    filterCollectionView.isHidden = true
+    resultLabel.isHidden = true
+    resultLabel.textColor = Themes.colorSolidWhite
+    resultLabel.font = Themes.fontRegularSubtitle
+    statusbarBackgroundView.backgroundColor = Themes.colorCharcoal
+    searchStationTextField.textColor = Themes.colorSolidWhite
+    searchStationTextField.font = Themes.fontRegularSubtitle
+    navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil) // with this we will disable back button label text
+  }
+  
+  func setupCustomSearchTextField() {
+    searchStationTextField.addTarget(self, action: #selector(filterTextEntered), for: .editingChanged) // add and observer for our custom searchBar
+    searchStationTextField.layer.cornerRadius = ObjectConstants.searchTextFiledBorderRadies
+    searchStationTextField.backgroundColor = Themes.colorDark
+    searchStationTextField.clipsToBounds = true // to make text field radiues rounded
+    searchStationTextField.layer.borderWidth = 2 // border width for the textfield
+    searchStationTextField.layer.borderColor = Themes.colorGrayScale.cgColor // border color for the textfield
+    searchStationTextField.attributedPlaceholder = NSAttributedString(
+      string:  "searchStationPlaceHolder".localizeString(),
+      attributes: [NSAttributedString.Key.foregroundColor: Themes.colorGrayScale]
+    )  // set textField placeholder color
+    searchStationTextField.setIcon(UIImage(systemName: "magnifyingglass")!) // set left view of the text field
+    let imageView = UIImageView(image: UIImage(systemName: "xmark.circle.fill")) // set right cancel button image
+    imageView.tintColor = Themes.colorGrayScale // set cancel button color
+    imageView.frame = CGRect(x: 0, y: 0, width: 25 , height: 25)
+    let paddingView: UIView = UIView.init(frame: CGRect(x: 0, y: 0, width: 40, height: 30))
+    paddingView.addSubview(imageView)
+    imageView.center = CGPoint(x: paddingView.frame.size.width  / 2,
+                               y: paddingView.frame.size.height / 2)
+    searchStationTextField.rightViewMode = .whileEditing // make cancel button visible when editing the text filed
+    searchStationTextField.rightView = paddingView
+    imageView.isUserInteractionEnabled = true
+    let cancelButton = UITapGestureRecognizer(target: self, action: #selector(clearTextField)) // add gesture func to our cancel button image
+    imageView.addGestureRecognizer(cancelButton)// add gesture to our cancel button image
+  }
+  
+  @objc
+  func filterTextEntered(textfield: UITextField) {
+    viewModel.filterTextValueEntered(textfield.text!) // call viewmodel Filter function
+  }
+  
+  @objc
+  func clearTextField() {
+    viewModel.resetCities() // reset the values of the our table view
+    searchStationTextField.text = "" // set our custom searchText' text empty
+    searchStationTextField.layer.borderColor = Themes.colorGrayScale.cgColor // border color for the textfield
+  }
     
 
 }
