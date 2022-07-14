3107d8a224b28311f5a7552493dd056afcd518b6
Berk <berkbeleli@gmail.com>
2022-07-15 02:51:40 +0300

    Created AppointmentDatas structure
    With this structure we will hold the selected station Datas like stationName, socketNumber, services also time and date selection datas

---
+++ Charger/Charger/Make Appointment/Model/AppointmentDatas.swift
@@ -0,0 +1,29 @@
+//
+//  AppointmentDatas.swift
+//  Charger
+//
+//  Created by Berk Beleli on 2022-07-15.
+//
+
+import Foundation
+
+struct AppointmentDatas {
+  
+  var address: String?
+  var workingHours: String?
+  var distance: String?
+  var stationCode: String?
+  var services: [String]?
+  
+  var stationID: Int?
+  var socketNumber: Int?
+  var deviceType: String?
+  var socketType: String?
+  var outsorcepower: String?
+  
+  var dateView: String?
+  var dateData: String?
+  var time: String?
+  var appointmentDuration: String?
+  
+}
