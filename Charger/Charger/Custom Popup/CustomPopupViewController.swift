dddd0e5a16efc296f670d74e791139c9d8526c86
Berk <berkbeleli@gmail.com>
2022-07-15 15:49:14 +0300

    Added vc Design

---
+++ Charger/Charger/Custom Popup/CustomPopupViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CustomPopupViewController.swift
+//  Charger
+//
+//  Created by Berk Beleli on 2022-07-15.
+//
+
+import UIKit
+
+class CustomPopupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+  
+    }
+
+
+}
