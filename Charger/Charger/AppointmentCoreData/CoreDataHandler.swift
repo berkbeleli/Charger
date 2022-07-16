c7608c3fb091fd37edfcc6b6f6b1b53ae36d2342
Berk <berkbeleli@gmail.com>
2022-07-17 01:18:39 +0300

    Deleted empty lines and added comment lines

--- Charger/Charger/AppointmentCoreData/CoreDataHandler.swift
+++ Charger/Charger/AppointmentCoreData/CoreDataHandler.swift
@@ -9,19 +9,16 @@ import UIKit
 import CoreData
 
 struct CoreDataHandler {
-  static var shared = CoreDataHandler()
+  static var shared = CoreDataHandler() // creates a singleton object
   var notifications: [NSManagedObject] = []
   /// This function saves the received notification values
   mutating func saveNotificationData(appointmentDate: String, appointmentTime: String, notificationTimer: String, notificationUniqueId: String, socketId: String, stationId: String) {
-    
     guard let appDelegate =
       UIApplication.shared.delegate as? AppDelegate else {
       return
     }
-    
     // 1 get the context
     let managedContext = appDelegate.persistentContainer.viewContext
-    
     // 2 get the core data object
     let entity = NSEntityDescription.entity(forEntityName: "AppointmentsCore",
                                  in: managedContext)!
@@ -45,42 +42,42 @@ struct CoreDataHandler {
       print("Could not save. \(error), \(error.userInfo)")
     }
   }
-  
+  /// Get the notification value according to choice
   mutating func catchAppoinmentNotificationData(returnType: String, appointmentDate: String, appointmentTime: String, socketId: String, stationId: String) -> String {
-    //1
       guard let appDelegate =
         UIApplication.shared.delegate as? AppDelegate else {
           return "could not catch"
       }
-      
+    // 1 get the context
       let managedContext =
         appDelegate.persistentContainer.viewContext
       
-      //2
+    // 2 get the core data object
       let fetchRequest =
         NSFetchRequest<NSManagedObject>(entityName: "AppointmentsCore")
       
-      //3
+    // 3 get the values
       do {
         self.notifications = try managedContext.fetch(fetchRequest)
         
       } catch let error as NSError {
         print("Could not fetch. \(error), \(error.userInfo)")
       }
+    
+    // filter the values according to the user choices
     let resultNotification = notifications.filter {
       ($0.value(forKey: "appointmentDate") as! String) == appointmentDate &&
       ($0.value(forKey: "appointmentTime") as! String) == appointmentTime &&
       ($0.value(forKey: "socketId") as! String) == socketId &&
       ($0.value(forKey: "stationId") as! String) == stationId
     }
-    if resultNotification.count > 0 {
-      if returnType == "notificationTime" {
+    if resultNotification.count > 0 { // check if there is any notification for that value
+      if returnType == "notificationTime" { // if user asks for notification timer value we will return it
         return (resultNotification[0].value(forKey: "notificationTimer") as? String)!
-      }else if returnType == "identifier" {
+      }else if returnType == "identifier" { // if the notification identifier requested
         return (resultNotification[0].value(forKey: "notificationUniqueId") as? String)!
       }
     }
-
     return ""
   }
   
--- Charger/Charger/Global Constants/Object Constants.swift
+++ Charger/Charger/Global Constants/Object Constants.swift
@@ -8,12 +8,10 @@
 import UIKit
 // With this global constant files we will reach object's constant values
 struct ObjectConstants {
-  
   static let buttonBorderRadius: CGFloat = 25.0
   static let filterbuttonBorderRadius: CGFloat = 16.0
   static let stationfilterbuttonBorderRadius: CGFloat = 14.0
   static let viewBorders: CGFloat = 6.0
   static let searchTextFiledBorderRadius: CGFloat = 20.0
   static let timeSelectionBorderRadius: CGFloat = 10.0
-  
 }
